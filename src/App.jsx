9a68c3efd9c3ec25cf3c08cdb891065d2982561c
kestulka <kgirdiunas0@gmail.com>
2024-03-06 16:06:13 +0200

    rendering lists

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,32 @@ import List from "./List.jsx";
 // ------------------------------------------
 
 function App() {
+  const fruits = [
+    { id: 1, name: "Orange", calories: 50 },
+    { id: 2, name: "Kiwi", calories: 60 },
+    { id: 3, name: "Melon", calories: 65 },
+    { id: 4, name: "Strawberries", calories: 30 },
+    { id: 5, name: "Pineapple", calories: 50 },
+    { id: 6, name: "Apple", calories: 120 },
+    { id: 7, name: "Banana", calories: 105 },
+    { id: 8, name: "Grapes", calories: 69 },
+    { id: 9, name: "Watermelon", calories: 130 },
+    { id: 10, name: "Peach", calories: 110 },
+  ];
+
+  const vegetables = [
+    { id: 1, name: "Carrot", calories: 41 },
+    { id: 2, name: "Broccoli", calories: 31 },
+    { id: 3, name: "Spinach", calories: 23 },
+    { id: 4, name: "Cucumber", calories: 16 },
+    { id: 5, name: "Bell Pepper", calories: 31 },
+    { id: 6, name: "Tomato", calories: 18 },
+    { id: 7, name: "Onion", calories: 40 },
+    { id: 8, name: "Lettuce", calories: 15 },
+    { id: 9, name: "Zucchini", calories: 17 },
+    { id: 10, name: "Mushroom", calories: 22 },
+  ];
+
   return (
     <>
       {/* Components */}
@@ -36,8 +62,11 @@ function App() {
       <UserGreeting isLoggedIn={true} username="kestulka" />
       <UserGreeting />
       {/* Render Lists */}
-      return(
-      <List items={fruits} category="Fruits" />)
+      {fruits.length > 0 && <List items={fruits} category="Fruits" />}
+      {vegetables.length > 0 && (
+        <List items={vegetables} category="Vegetables" />
+      )}
+      {/* jei listas butu tuscias, jo nerenderintu (short circuiting) */}
     </>
   );
 }
